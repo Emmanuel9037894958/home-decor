11c75c9f6225ca5a055cf9798f794df7a01c7134
Emmanuel9037894958 <emmanuelamamchukwu@gmail.com>
2026-06-11 22:35:02 +0100

    initial

--- src/components/WhatsAppFloat.jsx
+++ src/components/WhatsAppFloat.jsx
@@ -8,7 +8,17 @@ export default function WhatsAppFloat() {
       href="https://wa.me/2348037570385"
       target="_blank"
       rel="noopener noreferrer"
-      className="fixed right-5 bottom-10 z-[9999] bg-green-600 hover:bg-green-600 text-white p-4 rounded-full flex items-center justify-center transition-all duration-300 hover:scale-110 shadow-5xl animate-pulse"
+      className="
+        fixed right-2 bottom-32 z-[9999]
+        bg-green-600 hover:bg-green-700
+        text-white p-4 rounded-full
+        flex items-center justify-center
+        shadow-xl shadow-green-500/40
+        transition-all duration-300
+        hover:scale-110
+        animate-pulse
+        whatsapp-float
+      "
     >
       <FaWhatsapp size={28} />
     </a>
